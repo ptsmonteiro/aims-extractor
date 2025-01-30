aae33f48f82be64df0075fe930e0098ca181946b
Pedro <ptsmonteiro@gmail.com>
2025-01-30 22:34:01 +0100

    move CSV file path logic to main

--- csvlogs.py
+++ csvlogs.py
@@ -1,21 +1,12 @@
 import csv
-import glob
-import shutil
 
-def get_incoming_flights():
+def get_incoming_flights(file_path):
     flights = []
 
-    path = 'incoming/*.csv'
-    csv_files = glob.glob(path)
-
-    for f in csv_files:
-        print(f"Reading flights from {f}")
-        with open(f, newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                flights.append(row)
-
-        print(f"Moving {f} to processed folder...")
-        shutil.move(f, 'processed/')
+    print(f"Reading flights from {file_path}")
+    with open(file_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            flights.append(row)
 
     return flights
--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-import aims
 import extras
 import sheets
 import csvlogs
+import glob
+import shutil
 
 sheets.init()
-#from_date = sheets.get_last_entry_date()
 
-#aims.init()
-#flights = aims.get_flights(from_date)
-#aims.quit()
+# List files in incoming folder
+path = 'incoming/*.csv'
+csv_files = glob.glob(path)
 
-flights = csvlogs.get_incoming_flights()
-last = sheets.get_last_entry_and_subtotals()
-for f in flights:
-    last = sheets.insert_flight(extras.enhance_flight(f), last)
+# Process each file in the incoming folder
+for file_path in csv_files:
+
+    flights = csvlogs.get_incoming_flights(file_path)
+    last = sheets.get_last_entry_and_subtotals()
+    for f in flights:
+        last = sheets.insert_flight(extras.enhance_flight(f), last)
+
+    print(f"Moving {file_path} to processed folder...")
+    shutil.move(file_path, 'processed/')
